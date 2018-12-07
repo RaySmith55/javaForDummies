d2c991aee785840de6e9590f32dcf67309e7acf1
Ray Smith <ray.smith@sky.uk>
2018-12-07 17:16:30 +0000

    Add Millionaire.class

---
+++ src/main/java/Millionaire.java
@@ -0,0 +1,11 @@
+public class Millionaire {
+
+    public static void main (String args[]) {
+        double amountInAccount;
+
+        amountInAccount = 50.22;
+        amountInAccount = amountInAccount + 1000000.00;
+
+        System.out.println("You have $" + amountInAccount + " in your account.");
+    }
+}
